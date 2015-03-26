8c5230c03fb5f2e51622798ae5f9771b5071f9a8
Igor Akkerman <igor.akkerman@dynamite-services.de>
2015-03-26 19:30:27 +0100

    Project restructured for more modularization

---
+++ src/main/java/org/jlib/value/Accessor.java
@@ -0,0 +1,47 @@
+/*
+ * jlib - Open Source Java Library
+ *
+ *     www.jlib.org
+ *
+ *
+ *     Copyright 2005-2015 Igor Akkerman
+ *
+ *     Licensed under the Apache License, Version 2.0 (the "License");
+ *     you may not use this file except in compliance with the License.
+ *     You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *     Unless required by applicable law or agreed to in writing, software
+ *     distributed under the License is distributed on an "AS IS" BASIS,
+ *     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *     See the License for the specific language governing permissions and
+ *     limitations under the License.
+ */
+
+package org.jlib.value;
+
+import java.util.function.Supplier;
+
+/**
+ * Accessor of a {@link Value}. {@link Accessor#get()} differs from {@link Supplier#get()} by declaring the checked
+ * {@link NotAccessibleException}.
+ *
+ * @param <Value>
+ *        type of the value
+ *
+ * @author Igor Akkerman
+ */
+public interface Accessor<Value> {
+
+    /**
+     * Returns the accessed {@link Value}.
+     *
+     * @return accessed {@link Value}
+     *
+     * @throws NotAccessibleException
+     *         if no {@link Value} can be accessed
+     */
+    Value get()
+    throws NotAccessibleException;
+}
---
+++ src/main/java/org/jlib/value/OptionalModifiable.java
@@ -0,0 +1,76 @@
+/*
+ * jlib - Open Source Java Library
+ *
+ *     www.jlib.org
+ *
+ *
+ *     Copyright 2005-2015 Igor Akkerman
+ *
+ *     Licensed under the Apache License, Version 2.0 (the "License");
+ *     you may not use this file except in compliance with the License.
+ *     You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *     Unless required by applicable law or agreed to in writing, software
+ *     distributed under the License is distributed on an "AS IS" BASIS,
+ *     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *     See the License for the specific language governing permissions and
+ *     limitations under the License.
+ */
+
+package org.jlib.value;
+
+import org.checkerframework.checker.nullness.qual.Nullable;
+
+public class OptionalModifiable<Value>
+implements Modifiable<Value> {
+
+    public static <Value> OptionalModifiable<Value> from(final Value value) {
+        return new OptionalModifiable<>(value);
+    }
+
+    public static <Value> OptionalModifiable<Value> fromNullable(@Nullable final Value value) {
+        return value != null ?
+               from(value) :
+               new OptionalModifiable<>();
+    }
+
+    private Accessor<Value> delegateAccessor;
+
+    private final Accessor<Value> UNINITIALIZED = new Uninitialized<Value>() {
+
+        @Override
+        public void set(final Value value) {
+            delegateAccessor = new InitializedModifiable<>(value);
+        }
+    };
+
+    private OptionalModifiable() {
+        delegateAccessor = UNINITIALIZED;
+    }
+
+    private OptionalModifiable(final Value value) {
+        delegateAccessor = new Initialized<>(value);
+    }
+
+    @Override
+    public void set(final Value value) {
+        delegateAccessor = new Initialized<>(value);
+    }
+
+    public void unset() {
+        delegateAccessor = UNINITIALIZED;
+    }
+
+    @Override
+    public Value get()
+    throws NotAccessibleException {
+        return delegateAccessor.get();
+    }
+
+    @Override
+    public String toString() {
+        return delegateAccessor.toString();
+    }
+}
